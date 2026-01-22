1eaebcf23716ff651d83a05b094b7ee7c72007d7
Ariful3031 <hero.ariful31@gmail.com>
2026-01-22 19:35:27 +0600

    added to the color

---
+++ src/Components/Home/GradientColor.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GradientColor = () => {
+  return (
+    <div>
+        <p className='w-full h-10 bg-gradient-to-r from-blue-500 to-purple-500'>text</p>
+    </div>
+  )
+}
+
+export default GradientColor
--- src/Page/Home/Home.jsx
+++ src/Page/Home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import BookingCard from '../../Components/Home/BookingCard'
+import Test from '../../Components/Home/Color'
+import Color from '../../Components/Home/Color'
 
 const Home = () => {
   return (
-    <div>
+    <div className='min-h-[75vh]'>
         <BookingCard></BookingCard>
+        <Color></Color>
+        
+
     </div>
   )
 }
